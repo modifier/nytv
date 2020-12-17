1928ec5465295c7b100eea98816e2d9975615e10
Evgeniy Amiraslanov <the.modifikator@gmail.com>
2020-12-17 23:57:06 +0300

    Update layout by adding country codes

--- src/Channel.svelte
+++ src/Channel.svelte
@@ -1,6 +1,7 @@
 <script>
     import { nytvStore } from './store.js';
     import {onDestroy, onMount} from 'svelte';
+    import {getCountryName} from './countryNames';
 
     export let channel;
 
@@ -9,6 +10,8 @@
     let flagClass;
 
     let unsubscribe;
+
+    let countryName;
 
     onMount(() => {
         unsubscribe = nytvStore.subscribe(state => {
@@ -25,35 +28,49 @@
         nytvStore.setChannel(channel.id);
     }
 
-    $: flagClass = 'channel__lang em em-flag-' + channel.lang;
+    $: flagClass = 'channel__lang em em-flag-' + channel.countryCode;
+    countryName = getCountryName(channel.countryCode);
 </script>
 
 <div class="channel"
      class:channel--selected={$nytvStore.selectedChannel === channel.id}
      on:click={selectChannel}
      bind:this={channelElement}>
-    <i class={flagClass}></i> <span class="channel__name">{ channel.title }</span>
+    <div class="channel__title">
+        <span class="channel__name">{ channel.title }</span> <span class="channel__lang">({ channel.lang })</span>
+    </div>
+    <div class="channel__country">
+        <i class={flagClass}></i> { countryName }
+    </div>
 </div>
 
 <style>
     .channel {
-        padding: 0.4em 1em;
+        padding: 0.3em 1em 0.4em;
         cursor: pointer;
+    }
+
+    .channel:not(:last-child) {
+        margin-bottom: 0.2em;
     }
 
     .channel:hover {
         background: #f4f4f4;
     }
 
+    .channel__title {
+        padding: 0 0 0.2em 0;
+        font-size: 11px;
+    }
+
+    .channel__name {
+        font-size: 15px;
+        font-weight: bold;
+    }
+
     .channel--selected,
     .channel--selected:hover {
         background-color: lightsteelblue;
         cursor: default;
-    }
-
-    .channel__lang {
-        width: 1.2em;
-        height: 1.2em;
-        vertical-align: bottom;
     }
 </style>
--- src/channels.js
+++ src/channels.js
@@ -2,166 +2,192 @@ export default [
     {
         timezoneName: 'Europe/Dublin',
         title: 'TG4',
-        lang: 'ie',
+        countryCode: 'ie',
         url: 'https://www.tg4.ie/en/player/watch-live/home/',
-        video: 'https://csm-e-tg4ie.tls1.yospace.com/csm/live/195340990.m3u8'
+        video: 'https://csm-e-tg4ie.tls1.yospace.com/csm/live/195340990.m3u8',
+        lang: 'EN'
     },
     {
         timezoneName: 'Europe/London',
         title: 'Sky News',
-        lang: 'gb',
-        url: 'https://www.youtube.com/embed/9Auq9mYxFEE?autoplay=1'
+        countryCode: 'gb',
+        url: 'https://www.youtube.com/embed/9Auq9mYxFEE?autoplay=1',
+        lang: 'EN'
     },
     {
         timezoneName: 'Asia/Seoul',
         title: 'KBS',
-        lang: 'kr',
-        url: 'https://www.youtube.com/embed/4gwqZYZ-OSw?autoplay=1'
+        countryCode: 'kr',
+        url: 'https://www.youtube.com/embed/4gwqZYZ-OSw?autoplay=1',
+        lang: 'KO'
     },
     {
         timezoneName: 'Pacific/Fiji',
         title: 'Fiji TV',
-        lang: 'fj',
-        url: 'https://livestream.com/accounts/19514369/events/6947821/player?enableInfoAndActivity=true&defaultDrawer&autoPlay=true&mute=false'
+        countryCode: 'fj',
+        url: 'https://livestream.com/accounts/19514369/events/6947821/player?enableInfoAndActivity=true&defaultDrawer&autoPlay=true&mute=false',
+        lang: 'EN'
     },
     {
         timezoneName: 'Pacific/Auckland',
         title: 'Parliament',
-        lang: 'nz',
+        countryCode: 'nz',
         url: 'https://ondemand.parliament.nz',
-        video: 'https://ptvlive.kordia.net.nz/out/v1/daf20b9a9ec5449dadd734e50ce52b74/index.m3u8'
+        video: 'https://ptvlive.kordia.net.nz/out/v1/daf20b9a9ec5449dadd734e50ce52b74/index.m3u8',
+        lang: 'EN'
     },
     {
         timezoneName: 'Pacific/Guam',
-        title: 'Гуам',
-        lang: 'gu',
-        url: 'https://livestream.com/accounts/10612724/events/8102121/player?enableInfoAndActivity=true&defaultDrawer=&autoPlay=true'
+        title: 'Fox 6',
+        countryCode: 'gu',
+        url: 'https://livestream.com/accounts/10612724/events/8102121/player?enableInfoAndActivity=true&defaultDrawer=&autoPlay=true',
+        lang: 'EN'
     },
     {
         timezoneName: 'Australia/Sydney',
         title: 'ABC News',
-        lang: 'au',
-        url: 'https://www.youtube.com/embed/W1ilCy6XrmI?autoplay=1'
+        countryCode: 'au',
+        url: 'https://www.youtube.com/embed/W1ilCy6XrmI?autoplay=1',
+        lang: 'EN'
     },
     {
         timezoneName: 'Asia/Tokyo',
         title: 'NHK World',
-        lang: 'jp',
-        url: 'https://www3.nhk.or.jp/nhkworld/common/player/tv/live/embed/embed.html'
+        countryCode: 'jp',
+        url: 'https://www3.nhk.or.jp/nhkworld/common/player/tv/live/embed/embed.html',
+        lang: 'JP'
     },
     {
         timezoneName: 'Asia/Taipei',
         title: 'FTVN',
-        lang: 'tw',
-        url: 'https://www.youtube.com/embed/XxJKnDLYZz4?autoplay=1'
+        countryCode: 'tw',
+        url: 'https://www.youtube.com/embed/XxJKnDLYZz4?autoplay=1',
+        lang: 'CN'
     },
     {
         timezoneName: 'Asia/Shanghai',
         title: 'CGTN',
-        lang: 'cn',
+        countryCode: 'cn',
         url: 'https://www.cgtn.com/tv',
-        video: 'https://news.cgtn.com/resource/live/english/cgtn-news.m3u8'
+        video: 'https://news.cgtn.com/resource/live/english/cgtn-news.m3u8',
+        lang: 'EN'
     },
     {
         timezoneName: 'Asia/Dhaka',
         title: 'TV One',
-        lang: 'bd',
+        countryCode: 'bd',
         url: 'https://www.tvoneuk.tv',
-        video: 'https://cdn.appv.jagobd.com:444/c3VydmVyX8RpbEU9Mi8xNy8yMDE0GIDU6RgzQ6NTAgdEoaeFzbF92YWxIZTO0U0ezN1IzMyfvcGVMZEJCTEFWeVN3PTOmdFsaWRtaW51aiPhnPTI/tvoneuksni.stream/playlist.m3u8'
+        video: 'https://cdn.appv.jagobd.com:444/c3VydmVyX8RpbEU9Mi8xNy8yMDE0GIDU6RgzQ6NTAgdEoaeFzbF92YWxIZTO0U0ezN1IzMyfvcGVMZEJCTEFWeVN3PTOmdFsaWRtaW51aiPhnPTI/tvoneuksni.stream/playlist.m3u8',
+        lang: 'EN'
     },
     {
         timezoneName: 'Indian/Maldives',
         title: 'Maldives TV',
-        lang: 'mv',
+        countryCode: 'mv',
         url: 'https://maldivestv.mv',
-        video: 'https://58bdd3c8ef31c.streamlock.net/live/smil:maldivestv.smil/playlist.m3u8'
+        video: 'https://58bdd3c8ef31c.streamlock.net/live/smil:maldivestv.smil/playlist.m3u8',
+        lang: 'EN'
     },
     {
         timezoneName: 'Asia/Karachi',
         title: 'Indus News',
-        lang: 'pk',
-        url: 'https://www.youtube.com/embed/D9KHbHipoJ4?autoplay=1'
+        countryCode: 'pk',
+        url: 'https://www.youtube.com/embed/D9KHbHipoJ4?autoplay=1',
+        lang: 'EN'
     },
     {
         timezoneName: 'Asia/Karachi',
         title: 'ARY',
-        lang: 'pk',
-        url: 'https://live.arydigital.tv'
+        countryCode: 'pk',
+        url: 'https://live.arydigital.tv',
+        lang: 'EN'
     },
     {
         timezoneName: 'Asia/Tashkent',
         title: 'Uzreport TV',
-        lang: 'uz',
+        countryCode: 'uz',
         url: 'https://uzreport.news/live',
-        video: 'https://vspnmy3kvpzu7q.data.mediastore.eu-central-1.amazonaws.com/live.m3u8'
+        video: 'https://vspnmy3kvpzu7q.data.mediastore.eu-central-1.amazonaws.com/live.m3u8',
+        lang: 'EN'
     },
     {
         timezoneName: 'Asia/Dubai',
         title: 'Al Arabiya',
-        lang: 'ae',
-        url: 'https://www.youtube.com/embed/_M7Kcs8el-Q?autoplay=1'
+        countryCode: 'ae',
+        url: 'https://www.youtube.com/embed/_M7Kcs8el-Q?autoplay=1',
+        lang: 'AR'
     },
     {
         timezoneName: 'Asia/Dubai',
         title: 'Dubai One',
-        lang: 'ae',
+        countryCode: 'ae',
         url: 'https://www.awaan.ae/live/10/One-Tv',
-        video: 'https://dminnvll.cdn.mangomolo.com/dubaione/smil:dubaione.stream.smil/playlist.m3u8'
+        video: 'https://dminnvll.cdn.mangomolo.com/dubaione/smil:dubaione.stream.smil/playlist.m3u8',
+        lang: 'EN'
     },
     {
         timezoneName: 'Europe/Istanbul',
         title: 'TRT World',
-        lang: 'tr',
-        url: 'https://www.youtube.com/embed/CV5Fooi8YJA?autoplay=1'
+        countryCode: 'tr',
+        url: 'https://www.youtube.com/embed/CV5Fooi8YJA?autoplay=1',
+        lang: 'EN'
     },
     {
         timezoneName: 'Europe/Moscow',
         title: 'Channel One',
-        lang: 'ru',
-        url: 'https://www.1tv.ru/iframed/embedlive.html'
+        countryCode: 'ru',
+        url: 'https://www.1tv.ru/iframed/embedlive.html',
+        lang: 'RU'
     },
     {
         timezoneName: 'Europe/Moscow',
         title: 'BelRos',
-        lang: 'by',
+        countryCode: 'by',
         url: 'https://belros.tv',
-        video: 'https://live3.mediacdn.ru/sr1/tro/playlist.m3u8'
+        video: 'https://live3.mediacdn.ru/sr1/tro/playlist.m3u8',
+        lang: 'RU'
     },
     {
         timezoneName: 'Africa/Nairobi',
         title: 'Universal TV',
-        lang: 'so',
-        url: 'https://www.universaltvsomali.net/home'
+        countryCode: 'so',
+        url: 'https://www.universaltvsomali.net/home',
+        lang: 'EN'
     },
     {
         timezoneName: 'Africa/Nairobi',
         title: 'KTN News',
-        lang: 'ke',
-        url: 'https://www.youtube.com/embed/live_stream?channel=UCKVsdeoHExltrWMuK0hOWmg&autoplay=1'
+        countryCode: 'ke',
+        url: 'https://www.youtube.com/embed/live_stream?channel=UCKVsdeoHExltrWMuK0hOWmg&autoplay=1',
+        lang: 'EN'
     },
     {
         timezoneName: 'Africa/Johannesburg',
         title: 'CNBC',
-        lang: 'za',
-        url: 'https://www.youtube.com/embed/live_stream?channel=UCsba91UGiQLFOb5DN3Z_AdQ&autoplay=1'
+        countryCode: 'za',
+        url: 'https://www.youtube.com/embed/live_stream?channel=UCsba91UGiQLFOb5DN3Z_AdQ&autoplay=1',
+        lang: 'EN'
     },
     {
         timezoneName: 'Africa/Johannesburg',
         title: 'Gqama TV',
-        lang: 'za',
-        url: 'http://www.gqama.tv/live.php'
+        countryCode: 'za',
+        url: 'http://www.gqama.tv/live.php',
+        lang: 'EN'
     },
     {
         timezoneName: 'Africa/Cairo',
         title: 'Alghad TV',
-        lang: 'eg',
-        url: 'https://www.youtube.com/embed/s_lfbFjs_Yw?autoplay=1'
+        countryCode: 'eg',
+        url: 'https://www.youtube.com/embed/s_lfbFjs_Yw?autoplay=1',
+        lang: 'AR'
     },
     {
         timezoneName: 'Europe/Stockholm',
         title: 'Expressen TV',
-        lang: 'se',
+        countryCode: 'se',
         url: 'https://www.squidtv.net/europe/sweden/sweden-013.html',
-        video: 'https://httpcache0-03837-cachelive2.dna.ip-only.net/03837-cachelive2/smil:03837_tx2_720p/playlist.m3u8'
+        video: 'https://httpcache0-03837-cachelive2.dna.ip-only.net/03837-cachelive2/smil:03837_tx2_720p/playlist.m3u8',
+        lang: 'SE'
     },
 ];
